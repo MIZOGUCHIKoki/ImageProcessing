90b659e866d6574f86730259158493a4a4b6b95e
Koki Mizoguchi <250373b@ws.kochi-tech.ac.jp>
2022-10-17 16:04:14 +0900

    edited

---
+++ Exp_20221017/i1250373_kadai1.m
@@ -0,0 +1,18 @@
+clear;
+close all
+img_name = 'kut.jpg';
+output_name = 'input1_tonemapped.jpg';
+
+img = imread(img_name);
+
+%グレイスケール画像に変換
+gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
+%imshow(gimg);
+
+%入力画像のヒストグラムを出力
+result = tonecurve(gimg);
+imshow(result);
+
+function out = tonecurve(x)
+out=((255-128)/255)*x + 128;
+end
