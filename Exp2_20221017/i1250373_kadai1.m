cc7a8d404093276ed32f923c8da8da794282843e
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2022-10-20 21:06:06 +0900

    edited

--- Exp2_20221017/i1250373_kadai1.m
+++ Exp2_20221017/i1250373_kadai1.m
@@ -7,12 +7,49 @@ img = imread(img_name);
 
 %グレイスケール画像に変換
 gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
-%imshow(gimg);
 
-%入力画像のヒストグラムを出力
-result = tonecurve(gimg);
+[height,width] = size(gimg);
+outimg = zeros(height,width);
+hist = zeros(1,256);
+for k=0:255
+    count = 0;
+    for i = 1:height
+        for j = 1:width
+            if gimg(i,j) == k
+                count = count + 1; 
+            end
+        end
+    end
+end
+plot(0:255,hist);
+xlim([0 256]);
+map = zeros(256,1);
+oimg = zeros(height,width);
+
+for k=1:256
+    map(k) = (1.0/2)*(k-1) + 128;
+end
+
+for h = 1:height
+    for w = 1:width 
+        oimg(h,w) = map(gimg(h,w)+1);
+    end
+end
+
+result = unit8(oimg);
+figure;
 imshow(result);
 
-function out = tonecurve(x)
-out=((255-128)/255) * x + 128;
+count = zeros(1,256);
+for k = 0:255
+    for h = 1:height
+        for w = 1:width
+            if result(h,w) == k
+                count(k+1) = count(k+1) + 1;
+            end
+        end
+    end
 end
+figure;
+plot([0:255], count);
+xlim([0 255]);
--- Exp2_20221017/i1250373_kadai2.m
+++ Exp2_20221017/i1250373_kadai2.m
@@ -8,12 +8,11 @@ img = imread(img_name);
 %グレイスケール画像に変換
 gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
 %imshow(gimg);
-result = gimg / 64;
-result = result * 85;
+result = kaidan(gimg);
+
 imshow(result)
 [height, width] = size(result);
 count = zeros(1,256);
-
 for k = 0:255 % 0<=k<=255
     for h = 1:height
         for w = 1:width
@@ -24,5 +23,9 @@ for k = 0:255 % 0<=k<=255
     end
 end
 figure;
-plot([0:255], count);
+plot(0:255, count);
 xlim([0 255]);
+function out = kaidan(x)
+    out = x / 64;
+    out = out * 85; 
+end
