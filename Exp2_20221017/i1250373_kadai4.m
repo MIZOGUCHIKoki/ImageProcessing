ad50aa10f8d5eb0203bf3b6748ee33d1453b2f47
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2022-10-22 16:22:09 +0900

    edited

--- Exp2_20221017/i1250373_kadai4.m
+++ Exp2_20221017/i1250373_kadai4.m
@@ -6,26 +6,11 @@ img = imread(img_name);
 %グレイスケール画像に変換
 gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
 [height, width] = size(gimg);
-% グレイスケール画像のヒストグラムを出力
-% count = zeros(1,256);
-% for k = 0:255
-%     for h = 1:height
-%         for w = 1:width
-%             if gimg(h,w) == k
-%                 count(k+1) = count(k+1) + 1;
-%             end
-%         end
-%     end
-% end
-% % figure;
-% % plot([0:255], count);
-% % xlim([0 255]);
-
 map = zeros(256,1);
 oimg = zeros(height,width);
 
 for k=1:256
-    map(k) = 255/(150-25) * k - 15;
+    map(k) = 255/(150-25) * (k - 1) - 51;
 end
 figure('Name','課題4_トーンマップ');
 plot([0:255],map);
