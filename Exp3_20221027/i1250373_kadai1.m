4a0e23eef9c73a82063a5e4d38c91fedd8568dd5
Koki Mizoguchi <250373b@ws.kochi-tech.ac.jp>
2022-10-27 15:56:50 +0900

    executed successfully

---
+++ Exp3_20221027/i1250373_kadai1.m
@@ -0,0 +1,29 @@
+clear;
+close all;
+
+img_name = 'kut.jpg';
+
+img = imread(img_name);
+gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
+
+imshow(gimg);
+
+[height, width] = size(gimg);
+filter=ones(3)/9.0;
+
+oimg = zeros(height, width);
+
+
+for h = 2 : height - 1
+    for w = 2 : width - 1
+        for y = 1 : 3
+            for x = 1 : 3
+                oimg(h, w) = oimg(h, w) + gimg(h + y - 2, w + x - 2) * filter(y, x);
+            end
+        end
+    end
+end
+
+result = uint8(oimg);
+figure;
+imshow(result);
