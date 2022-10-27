654e8ae56bf098da5d1abb1e094a5afabd008e3d
Koki Mizoguchi <250373b@ws.kochi-tech.ac.jp>
2022-10-27 16:03:53 +0900

    changed comment add [cast uint 8 -> double]

--- Exp3_20221027/i1250373_kadai1.m
+++ Exp3_20221027/i1250373_kadai1.m
@@ -9,7 +9,7 @@ gimg = 0.3*img(:,:,1) + 0.59*img(:,:,2) + 0.11*img(:,:,3);
 imshow(gimg);
 
 [height, width] = size(gimg);
-filter=ones(3)/9.0;
+filter=ones(3,3)/9.0;
 
 oimg = zeros(height, width);
 
@@ -18,7 +18,8 @@ for h = 2 : height - 1
     for w = 2 : width - 1
         for y = 1 : 3
             for x = 1 : 3
-                oimg(h, w) = oimg(h, w) + gimg(h + y - 2, w + x - 2) * filter(y, x);
+                % double * uint8 = uint8になるから，事前にuint8をキャストする
+                oimg(h, w) = oimg(h, w) + double(gimg(h + y - 2, w + x - 2)) * filter(y, x);
             end
         end
     end
